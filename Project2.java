c2147a1836588a1ceed93b3be9d828e74f5b7e6e
Zhuoheng Li <40489953+Andy-LZH@users.noreply.github.com>
2019-03-25 13:34:35 +0800

    Add files via upload

---
+++ Project2.java
@@ -0,0 +1,17 @@
+package Project;
+
+public class Project2 {
+
+  public static void main(String[] args) {
+    int a = 0;
+    for(int i = 0; i < 100; i += 3){ /* problem while programming: i *= 3 (exponent) */
+
+      a += i;
+      System.out.println(a);
+
+    }
+
+
+
+  }
+}
